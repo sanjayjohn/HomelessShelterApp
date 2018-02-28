d89bf7a85b0467e3ca1c5891fce6cd0c663fd856
Robert Firstman <robfirstman@gmail.com>
2018-02-27 20:51:34 -0500

    Added basic login and registration functionality

--- node/2340/app.js
+++ node/2340/app.js
@@ -1,38 +1,73 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var firebase = require("firebase");
+var { firebaseConfig } = require("./config/config");
 
 var app = express();
 
+firebase.initializeApp(firebaseConfig);
+
 /*
 var logger = function(req, res, next){
-	console.log('logging');
-	next();
+    console.log('logging');
+    next();
 }
 
 app.use(logger);
 */
 
 // view engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Body parser middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Set static path
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', function(req, res){
-	res.render('index');
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+app.get("/register", function(req, res) {
+    res.render("registration");
+});
+
+app.get("/home", function(req, res) {
+    res.send("LOGGED IN");
 });
 
 app.post("/login", function(req, res) {
-    console.log("Login!");
-    console.log(req.body);
+    const { email, password } = req.body;
+    firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(() => {
+            res.redirect("/home");
+        })
+        .catch(err => {
+            console.log("LOGIN FAILED: ", err);
+            res.send("Login failed");
+        });
 });
 
-app.listen(3000, function(){
-	console.log('Hello World');
-})
+app.post("/register", function(req, res) {
+    const { email, password } = req.body;
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(() => {
+            res.redirect("/");
+        })
+        .catch(err => {
+            console.log("REGISTRATION FAILED: ", err);
+            res.send("Registration failed.");
+        });
+});
+
+app.listen(3000, function() {
+    console.log("Hello World");
+});
---
+++ node/2340/config/config.js
@@ -0,0 +1,11 @@
+// Contains configuration for application
+module.exports = {
+    firebaseConfig: {
+        apiKey: "AIzaSyBLnpArfvZG37DWb2w80Vr-JbRc0ea27fI",
+        authDomain: "m2firebaseproject-971cc.firebaseapp.com",
+        databaseURL: "https://m2firebaseproject-971cc.firebaseio.com",
+        projectId: "m2firebaseproject-971cc",
+        storageBucket: "m2firebaseproject-971cc.appspot.com",
+        messagingSenderId: "218525561932"
+    }
+}
